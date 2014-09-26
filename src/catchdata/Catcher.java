0b801000065b549fb5755804d61600e67db24c5a
haoliang.wu <haoliang.wu@liferay.com>
2014-09-26 17:11:33 +0800

    First Commit

---
+++ src/catchdata/Catcher.java
@@ -0,0 +1,635 @@
+package catchdata;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Catcher {
+	static WebDriver wd = null;
+	static WebElement we = null;
+	static String component = "";
+	static String componetLink = "";
+	static String caseName = "";
+	static String buildNum = "";
+	static List<String> buildNumList = new ArrayList<String>();
+	static File file;
+	static WritableWorkbook wwb = null;
+	static Label l = null;
+	static List<Integer> componetRowList = new ArrayList<Integer>();
+	static List<Integer> componetColList = new ArrayList<Integer>();
+	static List<Integer> caseNameRowList = new ArrayList<Integer>();
+	static List<Integer> caseNameColList = new ArrayList<Integer>();
+	static List<Integer> linkRowList = new ArrayList<Integer>();
+	static List<Integer> linkColList = new ArrayList<Integer>();
+	static String model_b = "false";
+	static String model_f = "false";
+	static String model_m = "false";
+	static String buildNumlist_parameter = "";
+	static String free_cases = "";
+	static int start = 0;
+	static String startName = "";
+	static int end = 0;
+	static String endName = "";
+	static int beginLine = 0;
+	static String url = "";
+	static boolean redo = true;
+	static List<List<String>> redoList = new ArrayList<List<String>>();
+	static int sheetNum = 0;
+
+	public static void setBuildNumlist_parameter(String buildNumlist_parameter) {
+		Catcher.buildNumlist_parameter = buildNumlist_parameter;
+	}
+
+	public static void setFree_cases(String free_cases) {
+		Catcher.free_cases = free_cases;
+	}
+
+	public static void setUrl(String url) {
+		Catcher.url = url;
+	}
+
+	public static void setBuildNum(String buildNum) {
+		Catcher.buildNum = buildNum;
+	}
+
+	public static void setBuildNumList(List<String> buildNumList) {
+		Catcher.buildNumList = buildNumList;
+	}
+
+	public static void setModel_b(String model_b) {
+		Catcher.model_b = model_b;
+	}
+
+	public static void setModel_f(String model_f) {
+		Catcher.model_f = model_f;
+	}
+
+	public static void setModel_m(String model_m) {
+		Catcher.model_m = model_m;
+	}
+
+	public static void setStart(int start) {
+		Catcher.start = start;
+	}
+
+	public static void setEnd(int end) {
+		Catcher.end = end;
+	}
+
+	public static void setBeginLine(int beginLine) {
+		Catcher.beginLine = beginLine;
+	}
+
+	public static void start() {
+
+		try {
+			Catcher.openWindow();
+
+			SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
+			String currentTime = sdf.format(new Date());
+			sdf = new SimpleDateFormat("yyyyMMdd");
+			String currentDate = sdf.format(new Date());
+
+			file = new File("out-put-result");
+			if (file == null) {
+				file.mkdirs();
+			}
+			file = new File("out-put-result\\" + currentDate + "-"
+					+ currentTime + ".xls");
+
+			wwb = init(file);
+
+			if (model_m != null && model_m.equals("true")) {
+				System.out.println("-----Build-model: mutiple model-----");
+				for (int i = 0; i < buildNumList.size(); i++) {
+					componetRowList.add(0);
+					componetColList.add(0);
+					caseNameRowList.add(0);
+					caseNameColList.add(1);
+					linkRowList.add(0);
+					linkColList.add(2);
+					// buildNum = buildNumList.get(i);
+					// System.out.println("-----#####-----This is " + buildNum
+					// + " script log-----#####-----");
+					// sheetNum = i;
+				}
+				Catcher.chooseComponent();
+			} else {
+				System.out.println("-----Build-model: sample model-----");
+				componetRowList.add(0);
+				componetColList.add(0);
+				caseNameRowList.add(0);
+				caseNameColList.add(1);
+				linkRowList.add(0);
+				linkColList.add(2);
+				// System.out.println(1111);
+
+				Catcher.chooseComponent();
+			}
+			wwb.write();
+			wwb.close();
+
+		} catch (Exception e) {
+			if (e.equals(TimeoutException.class)) {
+				e.printStackTrace();
+				System.out
+						.println("Current network-speed is slow, so close window in case of script stuck, please try again when net-work speed gets well.");
+			}
+		} finally {
+
+			if (redoList.get(0).size() != 0) {
+				try {
+					redo();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			try {
+				wd.quit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out
+						.println("Excel file input failed. Maybe it is caused by NULL writeable object or There is no error case. Please check it. ");
+
+			}
+		}
+
+	}
+
+	public static void openWindow() {
+		InputStream in = null;
+		Properties p = new Properties();
+		File file = new File("test.properties");
+		try {
+			in = new BufferedInputStream(new FileInputStream(file));
+			p.load(in);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		redoList.add(new ArrayList<String>());
+		redoList.add(new ArrayList<String>());
+
+		// url = p.getProperty("url");
+		String dir = p.getProperty("dir");
+		// model_b = p.getProperty("break-model");
+		// model_f = p.getProperty("free-model");
+		// model_m = p.getProperty("mutiple-model");
+
+		// free_cases = p.getProperty("free-cases");
+
+		if (model_m != null && model_m.equals("true")) {
+			buildNumList = divideBuildNumList(buildNumlist_parameter);
+		}
+		// else {
+		// buildNum = p.getProperty("build-num");
+		//
+		// }
+
+		// startName = p.getProperty("start-cname");
+		// endName = p.getProperty("end-cname");
+		// beginLine = Integer.parseInt(p.getProperty("begin-line"));
+
+		System.setProperty("webdriver.firefox.bin", dir);
+		wd = new FirefoxDriver();
+		wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		wd.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+
+		wd.navigate().to(url);
+
+	}
+
+	public static void setStartName(String startName) {
+		Catcher.startName = startName;
+	}
+
+	public static void setEndName(String endName) {
+		Catcher.endName = endName;
+	}
+
+	public static void redo() throws Exception {
+		System.out.println("##############################################");
+		System.out.println("This is redo-list " + redoList.get(1).toString());
+		String redoTemp = "";
+
+		for (String s : redoList.get(1)) {
+			redoTemp += s;
+			redoTemp += "/";
+		}
+		System.out.println("This is redo-list: " + redoTemp);
+
+		Catcher.free_toChooseBuild(redoTemp);
+
+	}
+
+	public static void chooseComponent() throws Exception {
+		try {
+			List<WebElement> list = wd.findElements(By
+					.xpath("//table[@id='projectstatus']/tbody/tr"));
+			if (model_b != null && model_b.equals("true")) {
+
+				List<WebElement> table_list = new ArrayList<WebElement>();
+				table_list = wd
+						.findElements(By
+								.xpath("//table[@id='projectstatus']/tbody/tr/td[3]/a[1]"));
+				for (int i = 1; i < table_list.size(); i++) {
+					String link = table_list.get(i).getText();
+					component = cutComponentName(link);
+					if (component.equals(startName)) {
+						start = i;
+					} else if (component.equals(endName)) {
+						end = i;
+					}
+				}
+				System.out.println("There are " + (end - start + 1)
+						+ " components");
+				System.out
+						.println("-----Component model: break-point model-----");
+				continuous_toChooseBuild(start + 2, end + 2);
+
+			} else if (model_f != null && model_f.equals("true")) {
+
+				System.out.println("-----Component model: free model-----");
+				free_toChooseBuild(free_cases);
+
+			} else {
+				start = 1 + beginLine;
+				end = list.size();
+				System.out.println("There are " + (end - start - 1)
+						+ " components");
+				System.out.println("-----Component model: default model-----");
+				continuous_toChooseBuild(start, end);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public static void free_toChooseBuild(String free_cases) throws Exception {
+		List<String> list = new ArrayList<String>();
+		if (model_f != null && model_f.equals("true")) {
+			list = divideFreeCases_redo(free_cases);
+		} else {
+			list = divideFreeCases(free_cases);
+		}
+		List<WebElement> table_list = new ArrayList<WebElement>();
+		table_list = wd.findElements(By
+				.xpath("//table[@id='projectstatus']/tbody/tr/td[3]/a[1]"));
+		for (int i = 1; i < table_list.size(); i++) {
+			table_list = wd.findElements(By
+					.xpath("//table[@id='projectstatus']/tbody/tr/td[3]/a[1]"));
+			String link = table_list.get(i).getText();
+			component = cutComponentName(link);
+			for (int j = 0; j < list.size(); j++) {
+				if (list.get(j).equals(component)) {
+					System.out.println("---");
+					System.out.println("Output the detail of the " + (j + 1)
+							+ "th case name:");
+					System.out.println(component + "--------component-name");
+					System.out.println(list.get(j) + "--------case-name");
+					System.out.println("Same, so enter the link..");
+					table_list.get(i).click();
+					we = table_list.get(i);
+
+					Catcher.chooseBuild();
+
+				}
+			}
+		}
+
+	}
+
+	public static List<String> divideFreeCases(String fc) {
+		List<String> list = new ArrayList<String>();
+		String tname;
+		String t_fc = fc;
+		while (!(t_fc.indexOf("/") == -1)) {
+			tname = t_fc.substring(0, t_fc.indexOf("/"));
+			t_fc = t_fc.substring(t_fc.indexOf("/") + 1);
+			list.add("portal-" + tname);
+		}
+
+		System.out.println("The free-cases list is " + list.toString());
+		return list;
+
+	}
+
+	public static List<String> divideFreeCases_redo(String fc) {
+		List<String> list = new ArrayList<String>();
+		String tname;
+		String t_fc = fc;
+		while (!(t_fc.indexOf("/") == -1)) {
+			tname = t_fc.substring(0, t_fc.indexOf("/"));
+			t_fc = t_fc.substring(t_fc.indexOf("/") + 1);
+			list.add(tname);
+		}
+
+		System.out.println("The free-cases list is " + list.toString());
+		return list;
+
+	}
+
+	public static List<String> divideBuildNumList(String bnl) {
+		List<String> list = new ArrayList<String>();
+		String tname;
+		String t_fc = bnl;
+		while (!(t_fc.indexOf("/") == -1)) {
+			tname = t_fc.substring(0, t_fc.indexOf("/"));
+			t_fc = t_fc.substring(t_fc.indexOf("/") + 1);
+			list.add(tname);
+		}
+
+		System.out.println("The build-num list is " + list.toString());
+		return list;
+	}
+
+	public static void continuous_toChooseBuild(int start, int end)
+			throws WriteException, BiffException, IOException {
+
+		for (int i = start; i <= end; i++) {
+
+			try {
+				we = wd.findElement(By
+						.xpath("//table[@id='projectstatus']/tbody/tr[" + i
+								+ "]/td[3]/a[1]"));
+				String link = we.getText();
+				component = cutComponentName(link);
+				System.out.println("-----");
+				System.out.print(component);
+
+				if (component.equals("portal-known-issues")
+						|| component.equals("portal-tools")
+						|| component.equals("portal-upgrades")
+						|| component.equals("portal-business-productivity-ee")) {
+					System.out.println("-----this component should be passed");
+					System.out.println("");
+					continue;
+				} else {
+					System.out.println("-----this component should be checked");
+					we.click();
+
+					if (model_m != null && model_m.equals("true")) {
+						Catcher.chooseMutipleBuild();
+					} else {
+						Catcher.chooseBuild();
+					}
+
+				}
+			} catch (Exception e) {
+				if (redo) {
+					System.out
+							.println("-----had re-do it but failed, so pass current component, the component name is "
+									+ component);
+
+					redoList.get(0).add(buildNum);
+					redoList.get(1).add(component);
+
+					wd.navigate().to(url);
+					continue;
+				}
+			}
+
+		}
+	}
+
+	public static String cutComponentName(String str) {
+		int i = str.indexOf('[');
+		int j = str.indexOf(']');
+		String cut = "error-component-name";
+		if (i != -1 && j != -1) {
+			cut = str.substring(i + 1, j);
+		}
+		return cut;
+	}
+
+	public static void chooseMutipleBuild() throws Exception {
+
+		for (int j = 0; j < buildNumList.size(); j++) {
+
+			buildNum = buildNumList.get(j);
+			System.out.println("-----#####-----This is " + buildNum
+					+ " script log-----#####-----");
+			sheetNum = j;
+
+			int num = 0;
+			List<WebElement> list = wd.findElements(By
+					.xpath("//table[@id='buildHistory']/tbody/tr"));
+			for (int i = 2; i < list.size(); i++) {
+				String temp = ConvertBuildNum(i);
+				if (buildNum.equals(temp)) {
+					num = i;
+					System.out.print("choose " + buildNum + " build");
+				}
+			}
+			we = wd.findElement(By
+					.xpath("//table[@id='buildHistory']/tbody/tr[" + num
+							+ "]/td[1]/a[1]/img"));
+			String flag = we.getAttribute("tooltip").substring(0, 8);
+			String flag1 = we.getAttribute("tooltip").substring(0, 6);
+			if (flag.equals("Unstable")) {
+				System.out.println("----------build is " + flag);
+				write(wwb, componetColList.get(j), componetRowList.get(j),
+						component);
+				we = wd.findElement(By
+						.xpath("//table[@id='buildHistory']/tbody/tr[" + num
+								+ "]/td[2]/a"));
+				we.click();
+				chooseErrorCase(j);
+				if (j == (buildNumList.size() - 1)) {
+					wd.navigate().back();
+				}
+			} else if (flag1.equals("Failed")) {
+				System.out.println("----------build is " + flag1);
+				write(wwb, componetColList.get(j), componetRowList.get(j),
+						component);
+				we = wd.findElement(By
+						.xpath("//table[@id='buildHistory']/tbody/tr[" + num
+								+ "]/td[2]/a"));
+				we.click();
+				chooseErrorCase(j);
+				if (j == (buildNumList.size() - 1)) {
+					wd.navigate().back();
+				}
+			} else if (flag.equals("Success ")) {
+				System.out.println("----------build is " + flag);
+				if (j == (buildNumList.size() - 1)) {
+					wd.navigate().back();
+				}
+
+			}
+
+		}
+
+	}
+
+	public static void chooseBuild() throws Exception {
+		int num = 0;
+		List<WebElement> list = wd.findElements(By
+				.xpath("//table[@id='buildHistory']/tbody/tr"));
+		for (int i = 2; i < list.size(); i++) {
+			String temp = ConvertBuildNum(i);
+			if (buildNum.equals(temp)) {
+				num = i;
+				System.out.print("choose " + buildNum + " build");
+			}
+		}
+		we = wd.findElement(By.xpath("//table[@id='buildHistory']/tbody/tr["
+				+ num + "]/td[1]/a[1]/img"));
+		String flag = we.getAttribute("tooltip").substring(0, 8);
+		String flag1 = we.getAttribute("tooltip").substring(0, 6);
+		if (flag.equals("Unstable")) {
+			System.out.println("----------build is " + flag);
+			write(wwb, componetColList.get(0), componetRowList.get(0),
+					component);
+			we = wd.findElement(By
+					.xpath("//table[@id='buildHistory']/tbody/tr[" + num
+							+ "]/td[2]/a"));
+			we.click();
+			chooseErrorCase(0);
+			wd.navigate().back();
+		} else if (flag1.equals("Failed")) {
+			System.out.println("----------build is " + flag1);
+			write(wwb, componetColList.get(0), componetRowList.get(0),
+					component);
+			we = wd.findElement(By
+					.xpath("//table[@id='buildHistory']/tbody/tr[" + num
+							+ "]/td[2]/a"));
+			we.click();
+			chooseErrorCase(0);
+			wd.navigate().back();
+		} else if (flag.equals("Success ")) {
+			System.out.println("----------build is " + flag);
+			wd.navigate().back();
+
+		}
+	}
+
+	public static String ConvertBuildNum(int line) {
+
+		we = wd.findElement(By.xpath("//table[@id='buildHistory']/tbody/tr["
+				+ line + "]/td[1]"));
+		if (we.getText().equals("")) {
+			return "empty";
+		} else {
+			return we.getText().substring(2, we.getText().length());
+
+		}
+
+	}
+
+	public static void chooseErrorCase(int a) throws Exception {
+		List<WebElement> list = wd.findElements(By
+				.xpath("//div[@id='matrix']/a"));
+		String flag = "";
+		for (int i = 1; i <= list.size(); i++) {
+			we = wd.findElement(By
+					.xpath("//div[@id='matrix']/a[" + i + "]/img"));
+			flag = we.getAttribute("tooltip");
+			we = wd.findElement(By.xpath("//div[@id='matrix']/a[" + i + "]"));
+			caseName = we.getText().substring(5, we.getText().length());
+			if (caseName.equals("lt")) {
+				caseName = "default";
+			}
+
+			if (flag.equals("Success ")) {
+				continue;
+			} else if (flag.equals("Failed ")) {
+				we.click();
+				System.out.print("The error case is " + caseName);
+				write(wwb, caseNameColList.get(a), caseNameRowList.get(a),
+						caseName);
+				caseNameRowList.set(a, caseNameRowList.get(a) + 1);
+				componetRowList.set(a, componetRowList.get(a) + 1);
+				// componetRow++;
+
+				componetLink = wd.getCurrentUrl();
+
+				System.out.println("--------record case link");
+				write(wwb, linkColList.get(a), linkRowList.get(a), componetLink);
+				linkRowList.set(a, linkRowList.get(a) + 1);
+				// linkRow++;
+
+				wd.navigate().back();
+			} else if (flag.equals("Unstable ")) {
+				we.click();
+				System.out.print("The error case is " + caseName);
+				write(wwb, caseNameColList.get(a), caseNameRowList.get(a),
+						caseName);
+				caseNameRowList.set(a, caseNameRowList.get(a) + 1);
+				componetRowList.set(a, componetRowList.get(a) + 1);
+
+				componetLink = wd.getCurrentUrl();
+
+				System.out.println("--------record case link");
+				write(wwb, linkColList.get(a), linkRowList.get(a), componetLink);
+				linkRowList.set(a, linkRowList.get(a) + 1);
+
+				wd.navigate().back();
+			}
+		}
+		wd.navigate().back();
+	}
+
+	public static WebDriver getWd() {
+		return wd;
+	}
+
+	public static void close(WebDriver wd) {
+		wd.quit();
+	}
+
+	public static void write(WritableWorkbook wwb, int x, int y, String str)
+			throws WriteException, IOException, BiffException {
+
+		WritableSheet ws = null;
+		if (wwb.getNumberOfSheets() == sheetNum) {
+			ws = wwb.createSheet(buildNum, sheetNum);
+		} else {
+			ws = wwb.getSheet(sheetNum);
+		}
+		l = new Label(x, y, str);
+
+		ws.addCell(l);
+		// if (wwb.getNumberOfSheets() == 0) {
+		// ws = wwb.createSheet("sheet1", 0);
+		// } else {
+		// ws = wwb.getSheet(0);
+		// }
+		// l = new Label(x, y, str);
+		//
+		// ws.addCell(l);
+
+	}
+
+	public static WritableWorkbook init(File file) throws IOException {
+		WritableWorkbook wwb = Workbook.createWorkbook(file);
+		return wwb;
+
+	}
+}
---
+++ src/userinterface/DocPanel.java
@@ -0,0 +1,322 @@
+package userinterface;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import catchdata.Catcher;
+
+public class DocPanel extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	GridBagConstraints c = new GridBagConstraints();
+	JTextField ffURL = new JTextField("https://build.liferay.com/2/view/test-portal-fixpack-frontend-tomcat-mysql%28ee-6.2.10_6.2.10.9%29/");
+	JTextField buildNum=new JTextField("#number");
+	JTextField begin_line=new JTextField("3");
+	JTextField buildNumList=new JTextField("#number/#number/(the last \""+"/"+"\" is important)");
+	JTextField startComponent=new JTextField("portal-component-name");
+	JTextField endComponent=new JTextField("portal-component-name");
+	JTextField freecase=new JTextField("name/name/(the last \""+"/"+"\" is important)");
+	JButton run_button=new JButton("Start");
+	JComboBox<String> b_model=new JComboBox<String>();
+	JComboBox<String> c_model=new JComboBox<String>();
+	JPanel mainPanel = new JPanel();
+	JPanel detail_panel=new JPanel();
+	ImageIcon background=new ImageIcon();
+	
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				DocPanel doc = new DocPanel();
+				doc.init();
+				doc.changeComponentModel();
+				doc.start();
+				
+			}
+		});
+	}
+
+	class main_Panel extends JPanel{
+		Image img;  
+		public main_Panel(){
+			background=new ImageIcon("avatar.png");  
+			img=background.getImage(); 
+			System.out.println(background.getIconHeight()+"-"+background.getIconWidth());
+		}
+		public void paintComponent(Graphics g)  
+		{   
+		super.paintComponent(g);  
+		
+		g.drawImage(img,0,0,null );  
+		}   
+		
+	}
+	
+	public void init() {
+		setTitle("GoogleDocGenarator");
+		setLocation(300, 300);
+		setSize(500, 300);
+		setResizable(false);
+		setPreferredSize(new Dimension(250, 250));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+
+		mainPanel.setBackground(Color.white);
+		mainPanel.setLayout(new GridBagLayout());
+		mainPanel.setBorder(BorderFactory.createTitledBorder("V15"));
+		
+		add(mainPanel);
+
+		b_model.addItem("Simple Mode");
+		b_model.addItem("Mutiple Mode(default mode only)");
+		c_model.addItem("Default Mode");
+		c_model.addItem("Break Mode");
+		c_model.addItem("Free Mode");
+		
+//		0,0	
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0;
+		c.weighty = 0;
+		mainPanel.add(new JLabel("Configuration"),c);
+		
+		c.gridy=1;
+		mainPanel.add(new JLabel("Job URL:"), c);
+
+//		1,1
+		c.gridx=1;
+		c.weightx=1;
+		mainPanel.add(ffURL, c);
+		
+//		0,2
+		c.gridx=0;
+		c.gridy=2;
+		c.weightx = 0;
+		mainPanel.add(new JLabel("Build Model:"),c);
+		
+//		1,2
+		JPanel model_panel=new JPanel();
+		model_panel.setLayout(new GridBagLayout());
+		model_panel.setBackground(Color.white);
+		c.gridx=0;
+		c.gridy=0;
+		model_panel.add(b_model,c);
+		c.gridx=2;
+		c.weightx=1;
+		model_panel.add(new JLabel(),c);
+		c.gridx=3;
+		c.weightx=0;
+		model_panel.add(run_button,c);
+		c.gridx=1;
+		c.gridy=2;
+		mainPanel.add(model_panel,c);
+		
+//		0,3
+		c.gridx=0;
+		c.gridy=3;
+		c.weightx = 0;
+		mainPanel.add(new JLabel("Component Model:"),c);
+		
+//		1,3
+		c.gridx=1;
+		c.gridy=3;
+		mainPanel.add(c_model, c);
+		
+//		0,4
+		c.gridx=0;
+		c.gridy=4;
+		c.weightx = 0;
+		mainPanel.add(new JLabel("Model Details:"),c);
+		
+//		1,4
+		
+		detail_panel.setLayout(new GridBagLayout());
+//		c.gridx=0;
+		c.gridy=0;
+		detail_panel.add(new JLabel("Build Number(Simple):"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(buildNum,c);
+		c.gridx=0;
+		c.gridy=1;
+		c.weightx=0;
+		detail_panel.add(new JLabel("Begin-Line:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(begin_line,c);
+		c.gridx=1;
+		c.gridy=4;
+		c.weighty=1;
+		mainPanel.add(detail_panel, c);
+	}
+	
+	public void changeComponentModel(){
+		c_model.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				detail_panel
+				if(c_model.getSelectedIndex()==0){
+					if(b_model.getSelectedIndex()==0){
+						defaultMode_s();
+					}
+					else if(b_model.getSelectedIndex()==1){
+						defaultMode_m();
+					}
+				}
+				else if(c_model.getSelectedIndex()==1){
+					breakMode();
+				}
+				else if(c_model.getSelectedIndex()==2){
+					freeMode();
+				}
+			}
+		});
+	}
+	private void init_model(){
+		Catcher.setModel_b("false");
+		Catcher.setModel_f("false");
+		Catcher.setModel_m("false");
+	}
+	
+	public void start(){
+		run_button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(c_model.getSelectedIndex()==0){
+					if(b_model.getSelectedIndex()==0){
+						init_model();
+						Catcher.setUrl(ffURL.getText());
+						Catcher.setBeginLine(Integer.parseInt(begin_line.getText()));
+						Catcher.setBuildNum(buildNum.getText());
+						
+						
+					}
+					else if(b_model.getSelectedIndex()==1){
+						init_model();
+						Catcher.setUrl(ffURL.getText());
+						Catcher.setBeginLine(Integer.parseInt(begin_line.getText()));
+						Catcher.setBuildNumlist_parameter(buildNumList.getText());
+						Catcher.setModel_m("true");
+					}
+				}
+				else if(c_model.getSelectedIndex()==1){
+					init_model();
+					Catcher.setUrl(ffURL.getText());
+					Catcher.setBuildNum(buildNum.getText());
+					Catcher.setStartName(startComponent.getText());
+					Catcher.setEndName(endComponent.getText());
+					Catcher.setModel_b("true");
+				}
+				else if(c_model.getSelectedIndex()==2){
+					init_model();
+					Catcher.setUrl(ffURL.getText());
+					Catcher.setBuildNum(buildNum.getText());
+					Catcher.setFree_cases(freecase.getText());
+					Catcher.setModel_f("true");
+				}
+				Catcher.start();
+			}
+		});
+	}
+	
+	
+	private void freeMode(){
+		b_model.setSelectedIndex(0);
+		detail_panel.removeAll();
+		detail_panel.revalidate();
+		c.gridx=0;
+		c.gridy=0;
+		detail_panel.add(new JLabel("Build Number(Simple):"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(buildNum,c);
+		c.gridx=0;
+		c.gridy=1;
+		detail_panel.add(new JLabel("Free Component List:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(freecase,c);
+	}
+	
+	private void breakMode(){
+		b_model.setSelectedIndex(0);
+		detail_panel.removeAll();
+		detail_panel.revalidate();
+		c.gridx=0;
+		c.gridy=0;
+		detail_panel.add(new JLabel("Build Number(Simple):"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(buildNum,c);
+		c.gridx=0;
+		c.gridy=1;
+		detail_panel.add(new JLabel("Start Component:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(startComponent,c);
+		c.gridx=0;
+		c.gridy=2;
+		detail_panel.add(new JLabel("End Component:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(endComponent,c);
+	}
+	private void defaultMode_m(){
+		detail_panel.removeAll();
+		detail_panel.revalidate();
+		c.gridx=0;
+		c.gridy=0;
+		detail_panel.add(new JLabel("Build Number(Mutiple):"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(buildNumList,c);
+		c.gridx=0;
+		c.gridy=1;
+		c.weightx=0;
+		detail_panel.add(new JLabel("Begin-Line:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(begin_line,c);
+		detail_panel.revalidate();
+	}
+	private void defaultMode_s(){
+		detail_panel.removeAll();
+		detail_panel.revalidate();
+		c.gridx=0;
+		c.gridy=0;
+		detail_panel.add(new JLabel("Build Number(Simple):"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(buildNum,c);
+		c.gridx=0;
+		c.gridy=1;
+		c.weightx=0;
+		detail_panel.add(new JLabel("Begin-Line:"),c);
+		c.gridx=1;
+		c.weightx=1;
+		detail_panel.add(begin_line,c);
+		detail_panel.revalidate();
+	}
+}
